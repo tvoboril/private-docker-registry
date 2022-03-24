595205f7a48093286bd21b5206194ea689f90dc8
Tomas Voboril <tom@tomvoboril.com>
2022-03-24 13:49:26 -0500

    adding tls settings

---
+++ makecert.sh
@@ -0,0 +1,8 @@
+openssl req -newkey rsa:4096 \
+            -x509 \
+            -sha256 \
+            -days 3650 \
+            -nodes \
+            -out priv.domain.crt \
+            -keyout priv.domain.key
+kubectl create secret tls domain-tls --key="priv.domain.key" --cert="priv.domain.crt"  --dry-run=client -o yaml >> /config/priv.domain-tls.yaml
