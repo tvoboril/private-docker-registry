6abf669a73b5b9f5d8043e7798d69aa340a09216
Tomas Voboril <tom@tomvoboril.com>
2022-04-10 21:09:53 -0600

    fixed a missing l

--- adduser.sh
+++ adduser.sh
@@ -5,4 +5,4 @@ export REG_HTPASSWD=$(cat htpasswd.auth | base64)
 
 head -21 registry-secrets.yaml > temp.registry-secrets.yaml
 echo "  HTPASSWD: "$(echo ${REG_HTPASSWD} | base64) >> temp.registry-secrets.yaml
-mv temp.registry-secrets.yam registry-secrets.yaml
+mv temp.registry-secrets.yaml registry-secrets.yaml
