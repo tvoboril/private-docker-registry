23db202fb2097d666b977662a0e52235b422dfa8
Tomas Voboril <tom@tomvoboril.com>
2022-04-11 21:27:14 -0600

    make consistant with catalog

--- adduser.sh
+++
@@ -1,8 +0,0 @@
-read -p "Registry User? : " REG_USER
-read -s -p "Registry Password? : " REG_PASSWD
-htpasswd -Bb htpasswd.auth $REG_USER $REG_PASSWD
-export REG_HTPASSWD=$(cat htpasswd.auth | base64)
-
-head -21 registry-secrets.yaml > temp.registry-secrets.yaml
-echo "  HTPASSWD: "$(echo ${REG_HTPASSWD} | base64) >> temp.registry-secrets.yaml
-mv temp.registry-secrets.yaml registry-secrets.yaml
